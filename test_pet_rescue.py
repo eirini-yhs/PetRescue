d27fcfa70fb56bb4d5471855ecb9ef0c6ee3c2f9
eirini-yhs <eirini.christou07@gmail.com>
2020-10-20 13:11:36 -0400

    Initial commit

---
+++ test_pet_rescue.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+import pet_rescue as pr
+
+
+class TestPetRescue(TestCase):
+    def test_add_pets_to_inventory(self):
+        awla = pr.PetRescue()
+
+        awla.add_pets_to_inventory("cats", 12)
+        num_pets = awla.get_pet_count("cats")
+        self.assertEquals(12, num_pets)
+
